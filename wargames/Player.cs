bce9d06ecea264cdadae925b9d2f0b91f1ea550b
ELeonJohnson <enochjohnsonleon@gmail.com>
2018-05-24 20:52:12 -0400

    started project

---
+++ wargames/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wargames
+{
+    class Player
+    {
+        private int _armyPoints;
+        private int _numberOfArmyBases;
+        private int _numberOfSoldiers;
+
+
+        public int ArmyPoints
+        {
+            get { return _armyPoints; }
+
+            set { _armyPoints = value; }
+        }
+
+        public int NumberOfArmyBases
+        {
+            get { return _numberOfArmyBases; }
+            set { _numberOfArmyBases = value; }
+        }
+
+        public int NumberOfSoldiers
+        {
+            get { return _numberOfSoldiers; }
+            set { _numberOfSoldiers = value; }
+        }
+    }
+}
---
+++ wargames/Soldier.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wargames
+{
+    class Soldier:Player 
+    {
+        private string _soldierRank;
+        private string _weapon;
+        private int _health;
+        private int _soldierStamina;
+        private int _attackDmg;
+        
+
+        public string SoldierRank
+        {
+            get { return _soldierRank; }
+            set { _soldierRank = value; }
+        }
+
+        public string Weapon
+        {
+            get { return _weapon; }
+            set { _weapon = value; }
+        }
+
+        public int Health
+        {
+            get { return _health; }
+
+            set
+            {
+                if (Health <= 0)
+                {
+                    _health = 0;
+                    Console.WriteLine($"{_soldierRank} has died.");
+                    NumberOfSoldiers = NumberOfSoldiers - 1;
+                } else if (Health >= 100) {
+                    _health = 100;
+                }
+            }
+        }
+
+        public int SoldierStamina
+        {
+            get { return _soldierStamina; }
+            set
+            {
+                if (SoldierStamina <= 0 || SoldierStamina > 50)
+                {
+                    _soldierStamina = 0;
+                    Console.WriteLine($"{_soldierRank} must rest before entering battle.");
+                } else if (SoldierStamina > 49)
+                {
+                    
+                }
+
+            }
+        }
+
+
+
+
+
+
+    }
+}
